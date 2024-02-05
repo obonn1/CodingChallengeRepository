103ffafa6de818a153aad81258a27a4bbcc23e2a
Omar Bonnet <omar.bonnet@gmail.com>
2024-02-05 10:38:00 -0500

    Added folder structure and first problem

---
+++ Codewars/kyu 5/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengeRepository.Codewars.kyu_5
+{
+    internal class Solution
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            //TODO : create code
+            return 0;
+        }
+    }
+}
