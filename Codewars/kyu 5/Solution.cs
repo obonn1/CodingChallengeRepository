784d384eac0af290994263a3ea84663bbc8ed0e8
Omar Bonnet <omar.bonnet@gmail.com>
2024-02-05 10:41:04 -0500

    Initial solution setup, not yet written

--- Codewars/kyu 5/Solution.cs
+++ Codewars/kyu 5/Solution.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CodingChallengeRepository.Codewars.kyu_5
 {
@@ -10,8 +11,9 @@ namespace CodingChallengeRepository.Codewars.kyu_5
     {
         public static int MaxSequence(int[] arr)
         {
-            //TODO : create code
-            return 0;
+            
+
         }
     }
 }
+
