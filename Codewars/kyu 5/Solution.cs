9fc18ca1ca23bc860485d0c24dc91751709563ea
Omar Bonnet <omar.bonnet@gmail.com>
2024-02-05 11:00:09 -0500

    Added link to the problem source

--- Codewars/kyu 5/Solution.cs
+++ Codewars/kyu 5/Solution.cs
@@ -11,8 +11,18 @@ namespace CodingChallengeRepository.Codewars.kyu_5
     {
         public static int MaxSequence(int[] arr)
         {
-            
-
+            var max = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                var sum = arr[i];
+                max = Math.Max(max, sum);
+                for (var j = i + 1; j < arr.Length; j++)
+                {
+                    sum += arr[j];
+                    max = Math.Max(max, sum);
+                }
+            }
+            return max;
         }
     }
 }
